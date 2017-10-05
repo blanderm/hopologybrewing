05efb8046a019f5081bfee53f399d7d154f74f8d
Bryan Landerman <bryan@dealer.com>
2017-10-05 14:28:54 -0400

    moved sns to notification function versus inline callback.

--- functions/brew_info_update/index.js
+++ functions/brew_info_update/index.js
@@ -2,8 +2,9 @@
 
 console.log('Loading brew_info_update function');
 
-var AWS = require('aws-sdk');
-var dynamo = new AWS.DynamoDB.DocumentClient();
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+const dynamo = new AWS.DynamoDB.DocumentClient();
 const CLICK_SINGLE = "SINGLE";
 const CLICK_DOUBLE = "DOUBLE";
 const CLICK_LONG = "LONG";
@@ -12,19 +13,6 @@ const IOT_SNS_TOPIC_ARN = process.env['IOT_BUTTON_ARN'];
 
 exports.handler = (event, context, callback) => {
     console.log("Request received: " + JSON.stringify(event));
-
-    let myCallback = function(msg) {
-        let sns = new AWS.SNS();
-
-        sns.publish({
-            Message: msg,
-            TopicArn: IOT_SNS_TOPIC_ARN
-        }, function(err, data) {
-            if (err) console.log(err, err.stack);
-            else console.log('Sent confirmation message: ' + msg);
-            callback(null, null);
-        });
-    };
 
     var brewDate = 0;
     dynamo.scan({TableName: BREW_INFO_TABLE_NAME}, function (err, resp) {
@@ -38,14 +26,14 @@ exports.handler = (event, context, callback) => {
             if ((item.brew_complete_date === undefined && now >= item.brew_date) || (now >= item.brew_date && now <= item.brew_complete_date)) {
                 brewDate = item.brew_date;
                 console.log("Brew date: " + item.brew_date);
-                processEvent(event, context, myCallback, brewDate);
+                processEvent(event, context, callback, brewDate);
                 break;
             }
         }
     });
 };
 
-function processEvent(event, context, callback, brewDate) {
+function processEvent(event, callback, brewDate) {
     let params = {
         Key: {
             brew_date: brewDate
@@ -84,6 +72,17 @@ function updateBrewInfo(params, attrName, callback) {
     dynamo.update(params, function (err, resp) {
         var respMsg = err ? err.message : "Persisted item with response " + JSON.stringify(resp) + " for item: \n" + paramsStr;
         console.log(respMsg);
-        callback(null, respMsg);
+        sendNotifiction(respMsg, callback);
+    });
+}
+
+function sendNotifiction(msg, callback) {
+    sns.publish({
+        Message: msg,
+        TopicArn: IOT_SNS_TOPIC_ARN
+    }, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else console.log('Sent confirmation message: ' + msg);
+        callback(null, msg);
     });
 }
