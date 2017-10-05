15f0746a77790aa6dfd9a55b39237d950e8cbf4a
Bryan Landerman <bryan@dealer.com>
2017-10-05 15:09:54 -0400

    moved from callback to sendNotification function.  Fixed bug not holding entire message.

--- functions/manage_cloudwatch_triggers/index.js
+++ functions/manage_cloudwatch_triggers/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('Loading manage_cloudwatch_triggers function');
+console.log('Loading ' + process.env['LAMBDA_FUNCTION_NAME'] + ' function');
 
 var AWS = require('aws-sdk');
 var cloudwatchevents = new AWS.CloudWatchEvents();
@@ -12,36 +12,22 @@ const IOT_SNS_TOPIC_ARN = process.env['IOT_BUTTON_ARN'];
 
 exports.handler = (event, context, callback) => {
     console.log("Request received: " + JSON.stringify(event));
-
-    let myCallback = function(msg) {
-        let sns = new AWS.SNS();
-
-        sns.publish({
-            Message: msg,
-            TopicArn: IOT_SNS_TOPIC_ARN
-        }, function(err, data) {
-            if (err) console.log(err, err.stack);
-            else console.log('Sent confirmation message: ' + msg);
-            callback(null, null);
-        });
-    };
-
     let msg = '';
 
     if (CLICK_SINGLE == event.clickType) {
         console.log("Received event type " + event.clickType + ", enabling pollers");
         msg = updateRule("every-minute", true, null, null);
-        updateRule("every-five-minutes", true, msg, myCallback);
+        updateRule("every-five-minutes", true, msg, callback);
     } else if (CLICK_DOUBLE == event.clickType) {
         console.log("Received event type " + event.clickType + ", disabling pollers");
         msg = updateRule("every-minute", false, null, null);
-        updateRule("every-five-minutes", false, msg, myCallback);
+        updateRule("every-five-minutes", false, msg, callback);
     } else if (CLICK_LONG == event.clickType) {
             console.log("Received event type " + event.clickType + ", disabling output poller");
-            updateRule("every-minute", false, null, myCallback);
+            updateRule("every-minute", false, null, callback);
     } else {
         console.log("Received event but ignoring.");
-        myCallback("Received event but ignoring.");
+        sendNotification("Received event but ignoring.", callback);
     }
 };
 
@@ -52,11 +38,11 @@ function updateRule(ruleName, enable, msg, callback) {
 
     if (enable) {
         cloudwatchevents.enableRule(params, function (err, data) {
-            handleRuleResult(err, data, "enabled", ruleName, msg, callback);
+            return handleRuleResult(err, data, "enabled", ruleName, msg, callback);
         });
     } else {
         cloudwatchevents.disableRule(params, function (err, data) {
-            handleRuleResult(err, data, "disabled", ruleName, msg, callback);
+            return handleRuleResult(err, data, "disabled", ruleName, msg, callback);
         });
     }
 }
@@ -66,8 +52,22 @@ function handleRuleResult(err, data, type, ruleName, msg, callback) {
     if (err) console.log(respMsg, err.stack);
     else console.log(respMsg);
 
-    msg = msg == null ? respMsg : msg += " // " + respMsg;
+    msg = msg == null ? respMsg : msg + " // " + respMsg;
 
-    if(callback != null) callback(msg);
+    if(callback != null) sendNotification(msg, callback);
     else return msg;
+}
+
+function sendNotification(msg, callback) {
+    let sns = new AWS.SNS();
+
+    sns.publish({
+        Message: msg,
+        TopicArn: IOT_SNS_TOPIC_ARN
+    }, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else console.log('Sent confirmation message: ' + msg);
+
+        if (callback) callback(null, msg);
+    });
 }
