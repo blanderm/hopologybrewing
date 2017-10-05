7e3b170d2f9d2fad23390aa18b6b6fc207fd691d
Bryan Landerman <bryan@dealer.com>
2017-10-05 14:11:58 -0400

    changed var to let

--- functions/brew_info_put/index.js
+++ functions/brew_info_put/index.js
@@ -6,16 +6,14 @@ const doc = require('dynamodb-doc');
 const dynamo = new doc.DynamoDB();
 
 exports.handler = (event, context, callback) => {
-    console.log("Request received: " + JSON.stringify(event));
-    var data = JSON.parse(event.body);
-    console.log("Body received: " + JSON.stringify(data));
-    var brewDate = (data.month !== undefined && data.day !== undefined && data.year !== undefined) ? new Date(data.year, data.month, data.day) : new Date();
+    let data = JSON.parse(event.body);
+    let brewDate = (data.month !== undefined && data.day !== undefined && data.year !== undefined) ? new Date(data.year, data.month, data.day) : new Date();
     brewDate.setUTCHours(4);
     brewDate.setUTCMinutes(0);
     brewDate.setUTCSeconds(0);
     brewDate.setUTCMilliseconds(0);
 
-    var params = {
+    let params = {
         TableName: "brew_info",
         Item: {
             brew_date: brewDate.getTime(),
@@ -31,13 +29,13 @@ exports.handler = (event, context, callback) => {
         });
     }
 
-    var paramsStr = JSON.stringify(params);
+    let paramsStr = JSON.stringify(params);
     console.log("Params to persist: " + paramsStr);
     dynamo.putItem(params, function (err, resp) {
         console.log(err ? err.message : "Persisted item with response " + JSON.stringify(resp) + " for item: \n" + paramsStr);
         callback(null, {
             statusCode: err ? '400' : '200',
-            body: err ? err.message : JSON.stringify({ message: 'Successfully persisted item', item: params.Item}),
+            body: err ? JSON.stringify(err.message) : JSON.stringify({ message: 'Successfully persisted item', item: params.Item}),
             headers: {
                 'Content-Type': 'application/json',
                 "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
