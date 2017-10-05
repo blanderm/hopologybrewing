5eb996cea9760ea51d9752940daf7b795d4ae2d6
Bryan Landerman <bryan@dealer.com>
2017-10-05 14:14:26 -0400

    cleaned up messaging to include a complete message for sns

--- functions/manage_cloudwatch_triggers/index.js
+++ functions/manage_cloudwatch_triggers/index.js
@@ -6,42 +6,68 @@ var AWS = require('aws-sdk');
 var cloudwatchevents = new AWS.CloudWatchEvents();
 const CLICK_SINGLE = "SINGLE";
 const CLICK_DOUBLE = "DOUBLE";
-const CLICK_LONG = "LONG"; // unused
+const CLICK_LONG = "LONG";
+const IOT_SNS_TOPIC_ARN = process.env['IOT_BUTTON_ARN'];
 
 
 exports.handler = (event, context, callback) => {
     console.log("Request received: " + JSON.stringify(event));
 
+    let myCallback = function(msg) {
+        let sns = new AWS.SNS();
+
+        sns.publish({
+            Message: msg,
+            TopicArn: IOT_SNS_TOPIC_ARN
+        }, function(err, data) {
+            if (err) console.log(err, err.stack);
+            else console.log('Sent confirmation message: ' + msg);
+            callback(null, null);
+        });
+    };
+
+    let msg = '';
+
     if (CLICK_SINGLE == event.clickType) {
         console.log("Received event type " + event.clickType + ", enabling pollers");
-        updateRule("every-minute", true);
-        updateRule("every-five-minutes", true, callback);
+        msg = updateRule("every-minute", true, null, null);
+        updateRule("every-five-minutes", true, msg, myCallback);
     } else if (CLICK_DOUBLE == event.clickType) {
         console.log("Received event type " + event.clickType + ", disabling pollers");
-        updateRule("every-minute", false);
-        updateRule("every-five-minutes", false, callback);
+        msg = updateRule("every-minute", false, null, null);
+        updateRule("every-five-minutes", false, msg, myCallback);
+    } else if (CLICK_LONG == event.clickType) {
+            console.log("Received event type " + event.clickType + ", disabling output poller");
+            updateRule("every-minute", false, null, myCallback);
     } else {
         console.log("Received event but ignoring.");
-        callback(null, null);
+        myCallback("Received event but ignoring.");
     }
 };
 
-function updateRule(ruleName, enable, callback) {
+function updateRule(ruleName, enable, msg, callback) {
     var params = {
         Name: ruleName
     };
 
     if (enable) {
         cloudwatchevents.enableRule(params, function (err, data) {
-            if (err) console.log(err, err.stack);
-            else console.log("Successfully enabled rule: " + params.Name);
-            if(callback) callback(null, null);
+            handleRuleResult(err, data, "enabled", msg, callback);
         });
     } else {
         cloudwatchevents.disableRule(params, function (err, data) {
-            if (err) console.log(err, err.stack);
-            else console.log("Successfully enabled rule: " + params.Name);
-            if(callback) callback(null, null);
+            handleRuleResult(err, data, "disabled", msg, callback);
         });
     }
+}
+
+function handleRuleResult(err, data, type, msg, callback) {
+    let respMsg = err ? err.message : "Successfully " + type + " rule: " + params.Name;
+    if (err) console.log(respMsg, err.stack);
+    else console.log(respMsg);
+
+    msg = msg == null ? respMsg : msg += " // " + respMsg;
+
+    if(callback) callback(msg);
+    else return msg;
 }
